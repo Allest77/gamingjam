1436741c2019a961e3794276717a17a323c572fc
Allest77 <30967524+Allest77@users.noreply.github.com>
2022-01-30 00:15:50 -0500

    Pause Menu

---
+++ smol/Assets/scripts/pauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public LayerMask UILayer;
+
+    void Start() {
+        Cursor.visible = true;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume(); //make a resume method
+            }
+            else {
+                Paws(); //make a pause method
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Paws() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
