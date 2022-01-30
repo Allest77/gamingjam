efdcebaa7a9b1e54a8dd129e4c1441fc4e39c097
Huniken123 <48298947+Huniken123@users.noreply.github.com>
2022-01-30 13:35:40 -0500

    Made a stop function

--- smol/Assets/scripts/AudioManager.cs
+++ smol/Assets/scripts/AudioManager.cs
@@ -53,6 +53,16 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
     }
 
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found!");
+        }
+        s.source.Stop();
+    }
+
 
 
 }
